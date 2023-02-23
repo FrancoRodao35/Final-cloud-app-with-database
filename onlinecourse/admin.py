87a4b542e1bf454413682c60ed8e8037b55c985d
FrancoRodao35 <francorodao35@gmail.com>
2023-02-23 19:47:05 -0300

    final commit

---
+++ onlinecourse/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+# <HINT> Import any new Models here
+from .models import Choice, Course, Lesson, Instructor, Learner, Question
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 5
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 5
+
+# Register your models here.
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+admin.site.register(Question)
+admin.site.register(Choice)
